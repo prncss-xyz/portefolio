bc1fe107e232555776cc60cbb9e2039bbd21864c
Juliette Lamarche <juliette.lamarche.xyz@gmail.com>
2024-05-13 17:34:26 -0400

    CI/CD: github pages

--- next.config.mjs
+++ next.config.mjs
@@ -6,9 +6,9 @@ import remarkMdxFrontmatter from "remark-mdx-frontmatter";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Configure `pageExtensions` to include markdown and MDX files
+  output: "export",
+  basePath: "/portefolio",
   pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
-  // Optionally, add any other Next.js config below
 };
 
 const withMDX = createMDX({
@@ -18,5 +18,4 @@ const withMDX = createMDX({
   },
 });
 
-// Merge MDX config with Next.js config
 export default withMDX(nextConfig);
