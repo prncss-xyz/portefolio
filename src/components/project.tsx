719bb55034883c0221c5368b17d7f8b91e7e894a
Juliette Lamarche <juliette.lamarche.xyz@gmail.com>
2024-05-13 15:45:28 -0400

    feat: thumbnails

--- src/components/project.tsx
+++ src/components/project.tsx
@@ -1,39 +1,24 @@
-import { Text, Card, Flex, Badge, Link } from "@radix-ui/themes";
-import { motion } from "framer-motion";
+import { Link, Text, Card, Flex, Badge, Box } from "@radix-ui/themes";
 import { ReactNode } from "react";
 import { ProjectTransition } from "./transitions";
+import Image from "next/image";
 
-function Ressource({ name, href }: { name: string; href?: string }) {
-  if (!href) return null;
-  return (
-    <Badge variant="outline" size="3">
-      <Link
-        underline="none"
-        style={{ color: "var(--accent-11)" }}
-        href={href}
-        target="_blank"
-      >
-        {name}
-      </Link>
-    </Badge>
-  );
-}
-
-const cardWidth = 300;
-const cardHeigh = 400;
+const cardWidth = 310;
+const cardHeigh = 310;
+const thumbSize = 100;
 
 export function Project({
   title,
   tags,
-  demo,
-  demoLabel,
+  site,
+  image,
   source,
   children,
 }: {
   title: string;
   tags: string[];
-  demo?: string;
-  demoLabel?: string;
+  site: string;
+  image: string;
   source?: string;
   children: ReactNode;
 }) {
@@ -47,19 +32,44 @@ export function Project({
           height: cardHeigh,
         }}
       >
-        <Flex direction="column" gap="3">
-          <Text weight="bold" style={{ color: "var(--accent-11)" }}>
-            {title}
-          </Text>
-          {children}
-          <Flex gap="1" wrap="wrap">
-            <Ressource name={demoLabel ?? "demo"} href={demo} />
-            <Ressource name="source" href={source} />
+        <Flex direction="column" gap="3" justify="between" height="100%">
+          <Flex direction="column" gap="3">
+            <Text weight="bold" style={{ color: "var(--accent-11)" }}>
+              {title}
+            </Text>
+            <Text style={{ color: "black" }}>{children}</Text>
           </Flex>
-          <Flex gap="1" wrap="wrap">
-            {tags.map((tag) => (
-              <Badge key={tag}>{tag}</Badge>
-            ))}
+          <Flex justify="between" gap="3" align="end">
+            <Flex direction="column" gap="3" align="start">
+              {source && (
+                <Link href={source} target="_blank">
+                  <Badge variant="outline" size="3">
+                    Source
+                  </Badge>
+                </Link>
+              )}
+              <Flex gap="1" wrap="wrap">
+                {tags.map((tag) => (
+                  <Badge key={tag}>{tag}</Badge>
+                ))}
+              </Flex>
+            </Flex>
+            <Flex justify="center">
+              <Link href={site} target="_blank">
+                <Box width={`${thumbSize}px`} height={`${thumbSize}px`}>
+                  <Image
+                    src={image}
+                    alt="thumbnail"
+                    style={{
+                      objectFit: "cover",
+                      width: "100%",
+                      height: "100%",
+                      borderRadius: "var(--radius-2)",
+                    }}
+                  />
+                </Box>
+              </Link>
+            </Flex>
           </Flex>
         </Flex>
       </Card>
