6e04a28a39e71b436d2d51f368304b998e1398c3
Juliette Lamarche <juliette.lamarche.xyz@gmail.com>
2024-05-14 08:54:25 -0400

    perf: lazy-load framer-motion

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,7 @@ import { Footer } from "@/components/footer";
 import { Banner } from "@/components/banner";
 import { Nav } from "@/components/nav";
 import { cssURL } from "@/utils/cssURL";
+import { MotionProvider } from "@/components/motionProvider";
 
 const font = Nunito({
   subsets: ["latin"],
@@ -27,37 +28,39 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={font.variable}>
-      <body
-        style={{
-          margin: 0,
-          // https://fffuel.co/llleaves/
-          backgroundImage: cssURL("/background.svg"),
-          width: "100vw",
-          height: "100vh",
-          backgroundRepeat: "repeat",
-          backgroundPositionX: "left",
-          backgroundPositionY: "top",
-        }}
-      >
-        <Theme
-          accentColor="cyan"
-          grayColor="mauve"
+    <MotionProvider>
+      <html lang="en" className={font.variable}>
+        <body
           style={{
-            backgroundColor: "transparent",
+            margin: 0,
+            // https://fffuel.co/llleaves/
+            backgroundImage: cssURL("/background.svg"),
+            width: "100vw",
+            height: "100vh",
+            backgroundRepeat: "repeat",
+            backgroundPositionX: "left",
+            backgroundPositionY: "top",
           }}
         >
-          <Container px={{ initial: "1", sm: "2", md: "4" }} pt="5" pb="3">
-            <Flex direction="column" gap="9">
-              <Banner />
-              <Nav />
-              {children}
-              <Footer />
-            </Flex>
-          </Container>
-          {themePanel && <ThemePanel />}
-        </Theme>
-      </body>
-    </html>
+          <Theme
+            accentColor="cyan"
+            grayColor="mauve"
+            style={{
+              backgroundColor: "transparent",
+            }}
+          >
+            <Container px={{ initial: "1", sm: "2", md: "4" }} pt="5" pb="3">
+              <Flex direction="column" gap="9">
+                <Banner />
+                <Nav />
+                {children}
+                <Footer />
+              </Flex>
+            </Container>
+            {themePanel && <ThemePanel />}
+          </Theme>
+        </body>
+      </html>
+    </MotionProvider>
   );
 }
