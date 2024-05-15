34703e3d63f2d3974ec4dd941050db6977f877b2
Juliette Lamarche <juliette.lamarche.xyz@gmail.com>
2024-05-15 10:58:04 -0400

    fix: transitions

--- src/components/transitions.tsx
+++ src/components/transitions.tsx
@@ -1,19 +1,19 @@
 "use client";
-
 import { m } from "framer-motion";
 import { ReactNode } from "react";
-
+import { MotionProvider } from "./motionProvider";
 
 export function ProjectTransition({ children }: { children: ReactNode }) {
   return (
-
-    <m.div
-      transition={{ duration: 0.7 }}
-      initial={{ opacity: 0 }}
-      whileInView={{ opacity: 1 }}
-      viewport={{ once: true }}
-    >
-      {children}
-    </m.div>
+    <MotionProvider>
+      <m.div
+        transition={{ duration: 0.7 }}
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        viewport={{ once: true }}
+      >
+        {children}
+      </m.div>
+    </MotionProvider>
   );
 }
