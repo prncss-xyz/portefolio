a6f7adae04bb37a82f1d500f5edc0c28d0666dfb
Juliette Lamarche <juliette.lamarche.xyz@gmail.com>
2024-05-19 17:02:49 -0400

    fix(about): avoid useless reloads by not serving a page at '/'

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -4,7 +4,7 @@ import { NavLink } from "./navLink";
 export function Nav() {
   return (
     <Root justify="end">
-      <NavLink href="/">about</NavLink>
+      <NavLink href="/about">about</NavLink>
       <NavLink href="/blog">blog</NavLink>
     </Root>
   );
